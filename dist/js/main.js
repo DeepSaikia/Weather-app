77829348fbd14730fa43aeea26dd0afde99d24b3
Deep Saikia <deep.saikia0047@gmail.com>
2021-06-06 12:52:00 +0530

    First commit

---
+++ dist/js/main.js
@@ -0,0 +1,22 @@
+import abc from './abc';
+
+const search = document.querySelector('.search');
+const searchBtn = document.querySelector('.search-btn');
+const closeBtn = document.querySelector('.close-btn');
+const searchInput = document.querySelector('.search-location');
+
+
+searchBtn.addEventListener('click', ev => {
+    search.classList.add('active');
+    searchInput.classList.add('active');
+    searchBtn.classList.add('active');
+    closeBtn.classList.add('active');
+    abc();
+})
+
+closeBtn.addEventListener('click', ev => {
+    search.classList.remove('active');
+    searchInput.classList.remove('active');
+    searchBtn.classList.remove('active');
+    closeBtn.classList.remove('active');
+})
